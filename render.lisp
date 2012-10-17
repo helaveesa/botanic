e3ea11d85cfb0985be2978a0e85554915e2f2e8f
helaveesa <helaveesa.nolder@gmail.com>
2012-10-17 23:52:38 +0400

    modified file render and templates add new templates and sections

--- render.lisp
+++ render.lisp
@@ -9,12 +9,26 @@
 (defparameter *default-render-method* (make-instance 'botanic-render))
 
 
-;; (defmethod restas:render-object ((designer botanic-render) (data list))
-;;   (destructuring-bind (headtitle navpoints content) data
-;;     (tpl:root (list :headtitle headtitle
-;;                     :content (tpl:base (list :navpoints navpoints
-;;                                              :content content
-;;                                              :stat (tpl:stat)))))))
+ (defmethod restas:render-object ((designer botanic-render) (data list))
+   (destructuring-bind (headtitle navpoints content) data
+     (tpl:root (list :headtitle headtitle
+                     :content (tpl:base (list :navpoints navpoints
+                                              :content content
+                                              :banner banner
+                                              :section1 section1
+                                              :textfeature textfeature
+                                              :featuretitle featuretitle
+                                              :link2 link2
+                                              :section2 section2
+                                              :text3 text3
+                                              :title3 title3
+                                              :link3 link3
+                                              :section3 section3
+                                              :text4 text4
+                                              :title4 title4
+                                              :link4 link4
+                                              :section4 section4
+                                              :stat (tpl:stat)))))))
 
 
 
