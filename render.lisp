48e9bf514a324c1453bc59713af747d5d93a3640
helaveesa <helaveesa.nolder@gmail.com>
2012-10-18 13:36:10 +0400

    comments

--- render.lisp
+++ render.lisp
@@ -9,21 +9,19 @@
 (defparameter *default-render-method* (make-instance 'botanic-render))
 
 
- (defmethod restas:render-object ((designer botanic-render) (data list))
-   (destructuring-bind (headtitle navpoints content) data
-     (tpl:root (list :headtitle headtitle
-                     :content (tpl:base (list :navpoints navpoints
-                                              :content content
-                                              :banner banner
-                                              :main main
-                                              :base base
-                                              :article article
-                                              :industry industry
-                                              :section1 section1
-                                              :textfeature textfeature
-                                              :featuretitle featuretitle
-                                              :link2 link2
-                                              :stat (tpl:stat)))))))
+ ;;(defmethod restas:render-object ((designer botanic-render) (data list))
+   ;;(destructuring-bind (headtitle navpoints content) data
+     ;;(tpl:root (list :headtitle headtitle
+                     ;;:content (tpl:base (list :navpoints navpoints
+                                              ;;:banner banner
+                                              ;;:main main
+                                              ;;:base base
+                                              ;;:article article
+                                              ;;:industry industry
+                                              ;;:section1 section1
+                                              ;;:textfeature textfeature
+                                              ;;:featuretitle featuretitle
+                                              ;;:stat (tpl:stat)))))))
 
 
 
