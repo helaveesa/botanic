16248d5f983fd280d1a6d9b647235ac73d4e2ac7
helaveesa <helaveesa.nolder@gmail.com>
2012-10-29 05:10:01 +0400

    modified file menufooter.lisp in process compiling

--- menufooter.lisp
+++ menufooter.lisp
@@ -1,15 +1,15 @@
 (in-package #:botanic)
 
 (defun menu ()
-  (list :menugroups
-        (mapcar #'(lambda (menugroup)
-                    (list :grouptitle (car menugroup) :grouplink (cadr menugroup) :left (caddr menugroup)
-                          :menublocks (mapcar #'(lambda (menublock)
+  ;;(list :menugroups
+        ;;(mapcar #'(lambda (menugroup)
+                    ;;(list :grouptitle (car menugroup) :grouplink (cadr menugroup) :left (caddr menugroup)
+                          ;;:menublocks (mapcar #'(lambda (menublock)
                                                   (list :headtitle (car menublock) :headlink (cadr menublock)
                                                         :menuitems (mapcar #'(lambda (menuitem)
                                                                                (list :title (car menuitem) :link (cadr menuitem)))
                                                                            (caddr menublock))))
-                                              (cadddr menugroup))))
+                                              ;;(cadddr menugroup))))
                 '(("Botanical.LAB"
                    "/about"
                     ("О компании" "/about_company")
