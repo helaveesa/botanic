4cd034dd5df723dc49c95c1f7029343ff2d1f1be
helaveesa <helaveesa.nolder@gmail.com>
2012-10-29 05:19:19 +0400

    oshibka in file menufooter

--- menufooter.lisp
+++ menufooter.lisp
@@ -4,11 +4,11 @@
   ;;(list :menugroups
         ;;(mapcar #'(lambda (menugroup)
                     ;;(list :grouptitle (car menugroup) :grouplink (cadr menugroup) :left (caddr menugroup)
-                          ;;:menublocks (mapcar #'(lambda (menublock)
-                                                  (list :headtitle (car menublock) :headlink (cadr menublock)
-                                                        :menuitems (mapcar #'(lambda (menuitem)
-                                                                               (list :title (car menuitem) :link (cadr menuitem)))
-                                                                           (caddr menublock))))
+                         ;;:menublocks (mapcar #'(lambda (menublock)
+                                                  ;;(list :headtitle (car menublock) :headlink (cadr menublock)
+                                                        (list :menuitems (mapcar #'(lambda (menuitem)
+                                                                               (list :title (car menuitem) :link (cadr menuitem))))))
+                                                                           ;;(caddr menublock)))))
                                               ;;(cadddr menugroup))))
                 '(("Botanical.LAB"
                    "/about"
